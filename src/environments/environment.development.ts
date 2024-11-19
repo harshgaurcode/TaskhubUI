88cf51408b7f1c4846b91f6874b8ae2a4dcbacca
Harsh gaur <harshgaur724@gmail.com>
2024-11-19 17:26:36 +0530

    updated Api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'https://localhost:7054',
+  // apiBaseUrl: 'https://localhost:7054',
+  apiBaseUrl:
+    'https://taskhubapi-gbg4d8ezdqgya2gy.canadaeast-01.azurewebsites.net',
 };
