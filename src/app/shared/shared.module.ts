f736f8d8778eb296739da84e56ae9de52a31cdbe
Harsh gaur <harshgaur724@gmail.com>
2024-11-20 12:34:00 +0530

    lets see

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { ModalComponent } from './modal/modal.component';
 import { ToastrModule } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NotFoundComponent, SpinnerComponent, ModalComponent],
-  imports: [CommonModule, ToastrModule],
+  imports: [CommonModule, ToastrModule, FormsModule],
   exports: [SpinnerComponent, ModalComponent],
 })
 export class SharedModule {}
